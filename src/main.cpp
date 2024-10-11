46682e46e5d7bdca93bd0073f39f0abb3d49f1cc
K0d3x 8its <k0d3x@pme.com>
2024-10-11 14:48:33 -0400

    improved comments to reflect pin use in relation to the DHT sensor

--- src/main.cpp
+++ src/main.cpp
@@ -19,16 +19,16 @@
 *                      [ ]-|GND         |____|                                *
 *              Ar.14 = [ ]-|A0            BAT|-[ ] = LiPo                     *
 *              Ar.15 = [ ]-|A1             EN|-[ ]                            *
-*              Ar.16 = [ ]-|A2           VUSB|-[ ] = VBUS                     *
+*              Ar.16 = [ ]-|A2           VUSB|-[*] ===> DHT22 Power           *
 *              Ar.17 = [ ]-|A3      *     D13|-[ ]                            * 
 *              Ar.18 = [ ]-|A4   Feather  D12|-[ ]                            * 
 *              Ar.19 = [ ]-|A5  M0 Proto  D11|-[ ] ~                          * 
 *        SCK = Ar.24 = [ ]-|D24 o o o o o D10|-[ ] ~                          *
 *       MOSI = Ar.23 = [ ]-|D23 o o o o o  D9|-[ ] ~                          * 
 *       MISO = Ar.22 = [ ]-|D22 o o o o o  D6|-[ ]                            *
-*                RX--> [ ]-|D0  o o o o o  D5|-[*] ~ ===> DHT22               * 
+*                RX--> [ ]-|D0  o o o o o  D5|-[*] ===> DHT22 Data ~          * 
 *                TX <--[ ]-|D1  o o o o o SCL|-[ ] = Ar.21                    * 
-*                      [*]-|GND o o o o o SDA|-[ ] = Ar.20                    * 
+*    DHT22 Ground <=== [*]-|GND o o o o o SDA|-[ ] = Ar.20                    * 
 *                           \_______________/                                 *                           
 *                                                                             *                               
 *******************************************************************************
